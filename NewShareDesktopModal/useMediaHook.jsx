931caacf362033cab19ebdad05029bf6cdc4a3be
Abdus Samad <imsamad00@gmail.com>
2022-11-14 14:17:16 +0530

    added variant

--- NewShareDesktopModal/useMediaHook.jsx
+++
@@ -1,7 +0,0 @@
-import { useMediaQuery } from "@material-ui/core";
-const useMediaHook = () => {
-  const isMdOrDown = useMediaQuery(`(max-width:800px)`);
-  return isMdOrDown;
-};
-
-export { useMediaHook };
